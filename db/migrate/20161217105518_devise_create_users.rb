31f5a3b35e0e6459f100b729e4f9c2f0a4626575
Taras Bardiuk <taras.bardyuk@gmail.com>
2016-12-17 19:57:46 +0200

    First user now created in seeds file

--- db/migrate/20161217105518_devise_create_users.rb
+++ db/migrate/20161217105518_devise_create_users.rb
@@ -39,11 +39,5 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
-
-    # Initialize first account:
-    User.create! do |u|
-      u.email     = 'test@test.com'
-      u.password  = 'password'
-    end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' },
 #   { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Initialize first account:
+User.create! do |u|
+  u.email     = 'test@test.com'
+  u.password  = 'password'
+end
