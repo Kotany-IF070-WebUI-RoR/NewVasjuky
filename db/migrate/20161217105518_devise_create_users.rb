4ccd22a5b322d7be847792ccb593667cf734d5d5
Taras Bardiuk <taras.bardyuk@gmail.com>
2016-12-17 19:57:46 +0200

    First user now created in seeds file

--- db/migrate/20161217105518_devise_create_users.rb
+++ db/migrate/20161217105518_devise_create_users.rb
@@ -39,6 +39,5 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
-
   end
 end
