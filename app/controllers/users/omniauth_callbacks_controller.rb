e95487dbe38739e1f12ca825efbbf22c3830ca62
f1nwe <kaban.volodymyr@gmail.com>
2016-12-18 16:21:21 +0200

    Error fixed in omniauth_callbacks_controller

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-class Users
+module Users
   class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     def facebook
       @user = User.from_omniauth(request.env['omniauth.auth'])
