cc122de4e9d5568e1aa727b50d6387059be546dd
mis6ko13 <mis6ko13@gmail.com>
2016-12-28 21:52:36 +0200

    fixed Gemfile.loc, added name validatiob with RefExp, fixed tests with pending

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,11 +1,14 @@
 class Issue < ApplicationRecord
   belongs_to :user
   belongs_to :category
+  REGEXP_NAME = /\p{L}/
   REGEXP_EMAIL = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.\w+\z/i
   REGEXP_PHONE = /\A[ x0-9\+\(\)\-\.]+\z/
   validates :name, :address, :phone, :email, :category, :description, :user_id,
             presence: true
-  validates :name, length: { maximum: 255 }
+  validates :name, length: { maximum: 255 },
+                   format: { with: REGEXP_NAME,
+                             message: 'should contain only letters' }
   validates :phone, length: { maximum: 50 },
                     format: { with: REGEXP_PHONE,
                               message: 'should contain only numbers' }
