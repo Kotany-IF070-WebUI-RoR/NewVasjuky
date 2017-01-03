64f84c4fa1a76002e361d49b8e7ba599c41552fd
f1nwe <kaban.volodymyr@gmail.com>
2017-01-03 19:43:57 +0200

    Cleaning admins issue controller and views from pundit features.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@
 class ApplicationController < ActionController::Base
   include Pundit
   include CheckAdminHelper
+  include ApplicationHelper
   helper_method :admin?, :admin_or_moderator?
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
   protect_from_forgery with: :exception
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,20 @@
 # frozen_string_literal: true
 module ApplicationHelper
+  def admin?
+    return if current_user && current_user.admin?
+    redirect_back(fallback_location: root_path)
+    flash[:alert] = 'Access denied'
+  end
+
+  def moderator?
+    return if current_user && current_user.moderator?
+    redirect_back(fallback_location: root_path)
+    flash[:alert] = 'Access denied'
+  end
+
+  def admin_or_moderator?
+    return if current_user && (current_user.admin? || current_user.moderator?)
+    redirect_back(fallback_location: root_path)
+    flash[:alert] = 'Access denied'
+  end
 end
