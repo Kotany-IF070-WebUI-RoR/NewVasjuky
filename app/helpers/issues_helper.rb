1b32461113ffd26e63682bf258fed7e0a54ffa37
Arseniy Shelestyuk <arsen325@gmail.com>
2017-01-21 14:30:39 +0200

    add popups to markers

--- app/helpers/issues_helper.rb
+++ app/helpers/issues_helper.rb
@@ -1,10 +1,14 @@
 module IssuesHelper
-
   def get_markers
     markers = []
-    @issues.each do |issue| 
-      markers << issue.geocode unless issue.geocode.nil?
+    @issues.each do |issue|
+      unless issue.geocode.nil?
+      marker = {}
+        marker['latlng'] = issue.geocode
+        marker['popup'] = link_to( issue.title, issue_path(issue))
+        markers << marker
+      end
     end
-    markers.to_json
+    markers.to_json.to_s.html_safe
   end
 end
