3aa5d207c7a8925916352597353cbbc4053696f1
f1nwe <kaban.volodymyr@gmail.com>
2017-02-01 19:19:37 +0200

    More DRY issue statuses

--- app/models/concerns/statuses.rb
+++ app/models/concerns/statuses.rb
@@ -1,9 +1,8 @@
 # Encoding: utf-8
 module Statuses
-  STATUSES = { 'opened' => 'Запит прийнято',
-               'pending' => 'Очікує на модерацію',
-               'declined' => 'Запит відхилено',
-               'closed' => 'Запит вирішено' }.freeze
-
-  STATUSES_SYM = [:pending, :declined, :opened, :closed].freeze
+  STATUSES = { opened: 'Запит прийнято',
+               pending: 'Очікує на модерацію',
+               declined: 'Запит відхилено',
+               closed: 'Запит вирішено' }.with_indifferent_access.freeze
+  STATUSES_SYM = STATUSES.keys.map(&:to_sym)
 end
