2caafd135adef7cfc8c020cf339ce2786177a88c
simplews <skipenko@simple.if.ua>
2017-02-12 13:07:32 +0200

    Shema

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema.define(version: 20170208080437) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "pg_trgm"
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
