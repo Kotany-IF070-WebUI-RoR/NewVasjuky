769650d5275a91e335f45711314f6cf2c0c3e9a1
f1nwe <kaban.volodymyr@gmail.com>
2017-02-13 19:49:37 +0200

    [BUGFIX] - load events when user does not have any notifications

--- app/helpers/feeds_helper.rb
+++ app/helpers/feeds_helper.rb
@@ -2,4 +2,8 @@ module FeedsHelper
   def users_feed?
     params[:controller] == 'feeds' && params[:action] == 'user_feed'
   end
+
+  def common_feed?
+    params[:controller] == 'feeds' && params[:action] == 'common_feed'
+  end
 end
--- spec/helpers/feeds_helper_spec.rb
+++ spec/helpers/feeds_helper_spec.rb
@@ -11,4 +11,16 @@ RSpec.describe FeedsHelper, type: :helper do
     controller.params[:action] = 'common_feed'
     expect(users_feed?).to be_falsey
   end
+
+  it 'Should return true when common_feed' do
+    controller.params[:controller] = 'feeds'
+    controller.params[:action] = 'common_feed'
+    expect(common_feed?).to be_truthy
+  end
+
+  it 'Should return true when not common_feed' do
+    controller.params[:controller] = 'feeds'
+    controller.params[:action] = 'user_feed'
+    expect(common_feed?).to be_falsey
+  end
 end
