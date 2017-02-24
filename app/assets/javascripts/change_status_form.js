4e53fa080c387765ce566de411ad7fa52c10178b
f1nwe <kaban.volodymyr@gmail.com>
2017-02-24 18:44:52 +0200

    fixed sending form. BUG cant send image with ajax

--- app/assets/javascripts/change_status_form.js
+++ app/assets/javascripts/change_status_form.js
@@ -1,6 +1,6 @@
 function init_change_status_form() {
     $("#add_event_description").on("ajax:success", function (e, data, status, xhr) {
-        location.reload()
+        location.reload();
     }).on("ajax:error", function (e, xhr, status, error) {
         remove_error_status(this);
         render_errors(xhr.responseText, this, 'event');
@@ -8,7 +8,7 @@ function init_change_status_form() {
 
     $( ".control_status_panel" ).on( "click", function( event ) {
         if ($(event.target).hasClass('change_status_link')){
-            var url = event.target.getAttribute('data-path')
+            var url = event.target.getAttribute('data-path');
             set_form_action(url)
         }
     });
