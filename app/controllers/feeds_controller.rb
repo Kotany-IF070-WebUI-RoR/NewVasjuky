f76bdcc58642071859f9e0ae52768644bae7ed72
f1nwe <kaban.volodymyr@gmail.com>
2017-02-25 11:58:01 +0200

    moved feed views to feed folder

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -25,7 +25,7 @@ class FeedsController < ApplicationController
   def respond_for_feed(events)
     if request.xhr?
       response.headers['TotalPages'] = events.total_pages
-      render partial: 'events/event_list'
+      render partial: 'feeds/events/event_list'
     else
       render 'feeds/feed'
     end
