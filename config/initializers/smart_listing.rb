9f6c6011191ac4dfc4b1a8b6a43d5b1f1b34ead8
simplews <skipenko@simple.if.ua>
2017-02-27 01:28:55 +0200

    Configured smart listing gem to use fontawesome icons

---
+++ config/initializers/smart_listing.rb
@@ -0,0 +1,10 @@
+SmartListing.configure do |config|
+  config.constants :classes, {
+    :icon_new              => "fa fa-plus",
+    :icon_edit             => "fa fa-pencil",
+    :icon_trash            => "fa fa-times",
+    :icon_sort_none        => "fa fa-sort",
+    :icon_sort_up          => "fa fa-sort-asc",
+    :icon_sort_down        => "fa fa-sort-desc",
+  }
+end
