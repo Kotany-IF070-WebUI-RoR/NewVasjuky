bd9f3276b2241322fd792bacd15c859c4e703bea
simplews <skipenko@simple.if.ua>
2017-02-28 10:19:45 +0200

    Fixed image size

--- app/uploaders/attachment_uploader.rb
+++ app/uploaders/attachment_uploader.rb
@@ -1,6 +1,6 @@
 class AttachmentUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
-  process resize_and_pad: [1002, 750]
+  process resize_to_fill: [1002, 750]
 
   # Choose what kind of storage to use for this uploader:
   if Rails.env.production?
