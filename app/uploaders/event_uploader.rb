ad0a1d2da2df2a905411167848546f3fddf12dbb
f1nwe <kaban.volodymyr@gmail.com>
2017-03-01 13:43:12 +0200

    [BUGFIX] -  S3 storage

--- app/uploaders/event_uploader.rb
+++ app/uploaders/event_uploader.rb
@@ -13,8 +13,7 @@ class EventUploader < CarrierWave::Uploader::Base
     process resize_to_fill: [800, 600]
   end
 
-  # Choose what kind of storage to use for this uploader:
-  storage :file
+  # Choose what kind of storage to use for this uploader
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
